4dae3cee2a8916329129b94dbc9a2939e00dc8de
Voldaev <lking13017@gmail.com>
2022-03-23 22:34:35 +0300

    асинхрон

--- src/main/java/MadTests/TestForum/event/UserRegistrationListener.java
+++ src/main/java/MadTests/TestForum/event/UserRegistrationListener.java
@@ -1,12 +1,15 @@
 package MadTests.TestForum.event;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserRegistrationListener {
+
     @EventListener
-    public void mailVerify(UserRegisteredPublished event) {
+    @Async
+    public void mailVerify(UserRegisteredPublished event) throws InterruptedException {
         System.out.println(event.getMail()+" отправить письмо на эту почту");
     }
 
